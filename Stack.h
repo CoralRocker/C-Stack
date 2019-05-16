1d61f1a8ed63ace404f5a952382155d772997a65
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-05-16 08:25:00 -0500

    Mod everything

--- Stack.h
+++ Stack.h
@@ -1,12 +1,44 @@
 #pragma once
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Stack {
-	int* stack;
+	int* _stack;
+	int _size;
 } Stack;
 
-Stack initStack(int val)
+Stack* initStack()
 {
-	Stack s = { val };
+	Stack *s = (Stack*)malloc(sizeof(Stack));
+	s->_stack = NULL;
+	s->_size = 0;
 	return s;
+}
+
+int topStack(Stack* s)
+{
+	return *s->_stack;
+}	
+
+void pushStack(Stack* s, int val)
+{
+	int* tmp = (int*)malloc(sizeof(int)*s->_size);
+	memcpy(tmp, s->_stack, sizeof(int)*s->_size);
+	free(s->_stack);
+	s->_stack = (int*)malloc(sizeof(int)*(s->_size+1));
+	s->_size++;
+	memcpy(s->_stack+1, tmp, sizeof(int)*(s->_size-1));
+	free(tmp);
+	s->_stack[0] = val;
+}
+
+void popStack(Stack* s)
+{
+	s->_size--;
+	int* tmp = (int*)malloc(sizeof(int)*s->_size-1);
+	memcpy(tmp, s->_stack+1, sizeof(int)*s->_size);
+	free(s->_stack);
+	s->_stack = (int*)malloc(sizeof(int)*s->_size);
+	memcpy(s->_stack, tmp, sizeof(int)*s->_size);
+	free(tmp);
 }
--- test.c
+++ test.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include "Stack.h"
 
+void printStack(Stack s)
+{
+	while(s._size != 0)
+	{
+		printf("%d\t", topStack(&s));
+		popStack(&s);
+	}
+}
+
 int main()
 {
-	Stack s = initStack(202);
+	Stack* s = initStack();
+	pushStack(s, 10);
+	pushStack(s, 30);
+	pushStack(s, 20);
+	pushStack(s, 5);
+	pushStack(s, 1);
+
+	printf("Stack: ");
+	printStack(*s);
+
+	printf("\nStack Size: %d\n", s->_size);
+	printf("Stack Top: %d\n", topStack(s));
+
+	printf("Stack Pop: ");
+	popSt
 }
